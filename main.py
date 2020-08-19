578421479e75b63c38d39886ee973412fe3c41b4
moundu <munenemuondu@gmail.com>
2020-08-19 12:02:33 +0300

    I have made some changes

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def shutdown(s):
+    """SHUTTING DOWN SYSTEM"""
+    if s == "yes":
+        # return "Shutting down"
+        print("Shutting down")
+    elif s == "no":
+        # return "Shutdown aborted"
+        print("Shutdown aborted")
+    else:
+        # return "Sorry"
+        print("Sorry")
+shutdown("yes")
+
